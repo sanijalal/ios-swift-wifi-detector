4d80de8904dda747e98b431e7834421405c137a3
Sani Jalal <binabdjalal@gmail.com>
2021-08-21 12:11:27 +0800

    Initial Commit

---
+++ WifiDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WifiDetector
+//
+//  Created by Sani on 21/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
